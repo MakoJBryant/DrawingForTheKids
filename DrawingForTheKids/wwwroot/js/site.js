490124652f4f757e11ba4f6eb88273c674691204
Mako Bryant <mako.j.bryant@outlook.com>
2019-08-20 10:44:07 -0700

    Bootstrap Carousel
     - Created carousel for home page with captions.
     - Created gallery template for showing drawings available for purchase.

--- DrawingForTheKids/wwwroot/js/site.js
+++ DrawingForTheKids/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$('#carousel-example-generic').carousel({
+    interval: 3000,
+    pause: null
+})
+
+/* Mobile touch support for carousel. */
+var touchSensitivity = 5;
+$(".carousel").on("touchstart", function (event) {
+    var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function (event) {
+        var xMove = event.originalEvent.touches[0].pageX;
+        if (Math.floor(xClick - xMove) > touchSensitivity) {
+            $(this).carousel('next');
+        } else if (Math.floor(xClick - xMove) < -(touchSensitivity)) {
+            $(this).carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function () {
+        $(this).off("touchmove");
+    });
+});
+
